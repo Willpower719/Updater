982d930eb90154781b8ec0c3195a332b50a4b1cc
Willpower719 <willrucker719@gmail.com>
2016-12-02 14:14:03 -0500

    Add files via upload

--- help.js
+++ help.js
@@ -2,9 +2,8 @@ $(function() {
    $(window).keypress(function(e) {
        var key = e.which;
        if (key == 27){
-            $( ".genericLoader" ).fadeOut( 500, function() {
-                dew.hide();
-            });
+           dew.hide();
+            };
        } else if (key ==96){
             dew.show("console");
        }
